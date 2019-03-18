8bcb8416356e61e3bcbc03cbe4832f2c67bf9250
Sweeny <44808412+MikeSweeny@users.noreply.github.com>
2019-03-18 12:11:34 -0400

    Woot finally got ch7-1

--- dailyWork_Day12/dailyProjects_chapter7/projectCh7-1/projectCh7-1/projectCh7-1.cpp
+++ dailyWork_Day12/dailyProjects_chapter7/projectCh7-1/projectCh7-1/projectCh7-1.cpp
@@ -1,21 +1,88 @@
-// projectCh7-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
+#include <string>
 #include <iostream>
+#include <vector>;
+using namespace std;
+
+class Enemy
+{
+
+public:
+
+	Enemy();
+	~Enemy();
+	static void DisplayTactics();
+	static void SetNumEnemies(int addRemove);
+
+private:
+
+	static int numEnemies;
+
+};
+
+Enemy::Enemy()
+{
+	SetNumEnemies(1);
+}
+
+Enemy::~Enemy()
+{
+	SetNumEnemies(0);
+}
+
+void Enemy::SetNumEnemies(int addRemove)
+{
+	if (addRemove == 1)
+	{
+		numEnemies++;
+	}
+	if (addRemove == 0)
+	{
+		numEnemies--;
+	}
+}
+
+void Enemy::DisplayTactics()
+{
+	if (numEnemies < 5)
+	{
+		cout << "Engaging Defensive Tactics Set.\n";
+	}
+	else
+	{
+		cout << "Engaging Aggressive Tactics Set.\n";
+	}
+}
+
+
+
+
+void CreateEnemies()
+{
+	Enemy* nme1 = new Enemy();
+	Enemy* nme2 = new Enemy();
+	Enemy::DisplayTactics();
+	delete nme1;
+	delete nme2;
+}
+
+int Enemy::numEnemies = 0;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	// Number of entires to create
+	const int NUM1 = 3;
+	cout << "In main()\n";
+	cout << "Creating \n" << NUM1 << " enemy objects.\n";
+	Enemy group1[NUM1];
+	// Should display defensive tactics
+ 	Enemy::DisplayTactics();
+	// Should display aggressive tactics
+	CreateEnemies();
+
+	cout << "\nBack in main().\n";
+	// Should display defensive tactics
+	Enemy::DisplayTactics();
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
